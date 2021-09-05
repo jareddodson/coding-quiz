56efb7a8825cb0902c09b112ec52397c7f070c9a
jareddodson <jared.dodson04@gmail.com>
2021-09-05 12:17:56 -0500

    updating js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,51 @@
-var myQuestions = [
+var startBtn = document.getElementById("startBtn");
+var nextButton = document.getElementById("nextButton")
+
+var QQ = document.getElementById("Qs");
+var A = document.getElementById("a");
+var B = document.getElementById("b");
+var C = document.getElementById("c");
+var D = document.getElementById("d");
+
+var questions = [
     {
-        question: "What does DOM stand for?",
-        answers: {
-            A: 'Display Or Make' ,
-            B: 'Document Object Material' ,
-            C: 'Doccument Object Model' ,
-            D: 'Dont Object Main'
-        }, 
-        correctAnswer: 'C'
+        q: "What does DOM stand for?",
+        a: ["Display Or Make", "Document Object Material", "Document Object Model", "Dont Object Main"],
+        correct: "Document Object Model"
     },
     {
-        question: "What does var mean?",
-        answers: {
-            A: 'Variety' ,
-            B: 'Variable' ,
-            C: 'Various' ,
-            D: 'Variform' 
-        },
-        correctAnswer: 'B'
+        q: "What does var mean?",
+        a: ["Variety", "Variable", "Various", "Variform"],
+        correct: "Variable"
     },
     {
-        question: "Which method saves data to localStorage: setItem() or getItem()?",
-        answers: {
-            A: 'setItem()' ,
-            B: 'getItem()'
-        },
-        correctAnswer: 'A'
+        q: "Which method saves data to localStorage: setItem() or getItem()?",
+        a: ["setItem()", "getItem()"],
+        correct: "setItem()"
     }
 ];
+
+function quiz() {
+    document.getElementById("startBtn").style.display = "none";
+    document.getElementById("questions").style.display = "block";
+    document.getElementById("intro").style.display = "none";
+    question();
+}
+
+function question() {
+    QQ.textContent = questions[0].q;
+    a.textContent = questions[0].a[0];
+    b.textContent = questions[0].a[1];
+    c.textContent = questions[0].a[2];
+    d.textContent = questions[0].a[3];
+
+    for (var i = 0; i < questions.length; i++) {
+        var obj = questions[i];
+    }
+}
+
+
+
+nextButton.onclick = quiz;
+
+startBtn.onclick = quiz;
