af49df67bbe579f03a6d2526a16ce79a3d8b8020
jareddodson <jared.dodson04@gmail.com>
2021-09-05 10:29:22 -0500

    Adding var myQuestions

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var myQuestions = [
+    {
+        question: "What does DOM stand for?",
+        answers: {
+            A: 'Display Or Make' ,
+            B: 'Document Object Material' ,
+            C: 'Doccument Object Model' ,
+            D: 'Dont Object Main'
+        }, 
+        correctAnswer: 'C'
+    },
+    {
+        question: "What does var mean?",
+        answers: {
+            A: 'Variety' ,
+            B: 'Variable' ,
+            C: 'Various' ,
+            D: 'Variform' 
+        },
+        correctAnswer: 'B'
+    },
+    {
+        question: "Which method saves data to localStorage: setItem() or getItem()?",
+        answers: {
+            A: 'setItem()' ,
+            B: 'getItem()'
+        },
+        correctAnswer: 'A'
+    }
+];
