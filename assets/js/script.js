6bea7ec6aec063cced471e2a42a9b1d76896165f
jareddodson <jared.dodson04@gmail.com>
2021-09-05 21:33:37 -0500

    updating html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,6 +48,10 @@ function quiz() {
     document.getElementById("questions").style.display = "block";
     document.getElementById("intro").style.display = "none";
     question();
+
+    if (a === correct) {
+        console.log('correct')
+    }
 }
 
 function question() {
@@ -57,11 +61,40 @@ function question() {
     c.textContent = questions[0].a[2];
     d.textContent = questions[0].a[3];
 
+    QQ.textContent = questions[1].q;
+    a.textContent = questions[1].a[0];
+    b.textContent = questions[1].a[1];
+    c.textContent = questions[1].a[2];
+    d.textContent = questions[1].a[3];
+    
+    QQ.textContent = questions[2].q;
+    a.textContent = questions[2].a[0];
+    b.textContent = questions[2].a[1];
+    c.textContent = questions[2].a[2];
+    d.textContent = questions[2].a[3];
+
+    
+
     for (var i = 0; i < questions.length; i++) {
         var obj = questions[i];
     }
 }
 
+function changeQuestion() {
+
+    A.onclick = changeQuestion;
+    B.onclick = changeQuestion;
+    C.onclick = changeQuestion;
+    D.onclick = changeQuestion;
+
+    console.log("Answer clicked!");
+
+    question();
+}
+
 startBtn.onclick = timer;
 
-
+A.onclick = changeQuestion;
+B.onclick = changeQuestion;
+C.onclick = changeQuestion;
+D.onclick = changeQuestion;
